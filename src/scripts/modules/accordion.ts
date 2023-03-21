0275b54f792dc321a23f583565dc16b4bc2dd41b
David Bergmann <david.bergmann@verndale.com>
2023-03-21 17:38:32 -0500

    Bootstrap like grid

--- src/scripts/modules/accordion.ts
+++ src/scripts/modules/accordion.ts
@@ -1,25 +1,25 @@
-import { Component } from "@verndale/core";
-import "../../scss/modules/accordion.scss";
+import { Component } from '@verndale/core';
+import '../../scss/modules/accordion.scss';
 
 class Accordion extends Component {
   setupDefaults() {
     this.dom = {
-      triggers: this.el.querySelectorAll(".accordion__item-trigger"),
-      panels: this.el.querySelectorAll(".accordion__item-panel"),
+      triggers: this.el.querySelectorAll('.accordion__item-trigger'),
+      panels: this.el.querySelectorAll('.accordion__item-panel')
     };
 
     this.initAccordion();
   }
 
   addListeners() {
-    (this.dom.triggers as NodeList).forEach((trigger) =>
-      trigger.addEventListener("click", this.handleClick.bind(this))
+    (this.dom.triggers as NodeList).forEach(trigger =>
+      trigger.addEventListener('click', this.handleClick.bind(this))
     );
   }
 
   initAccordion() {
-    (this.dom.triggers as NodeListOf<HTMLButtonElement>).forEach((trigger) => {
-      if (trigger.getAttribute("aria-expanded") === "true") {
+    (this.dom.triggers as NodeListOf<HTMLButtonElement>).forEach(trigger => {
+      if (trigger.getAttribute('aria-expanded') === 'true') {
         this.openItem(trigger);
       }
     });
@@ -28,7 +28,7 @@ class Accordion extends Component {
   handleClick(e: Event) {
     const trigger = e.currentTarget as HTMLButtonElement;
 
-    if (trigger.getAttribute("aria-expanded") === "true") {
+    if (trigger.getAttribute('aria-expanded') === 'true') {
       this.closeItem(trigger);
     } else {
       this.openItem(trigger);
@@ -36,36 +36,36 @@ class Accordion extends Component {
   }
 
   openItem(trigger: HTMLButtonElement) {
-    trigger.setAttribute("aria-expanded", "true");
+    trigger.setAttribute('aria-expanded', 'true');
 
     const panel = document.getElementById(
-      trigger.getAttribute("aria-controls") as string
+      trigger.getAttribute('aria-controls') as string
     );
 
     if (panel) {
       const pannelContent = panel.querySelector<HTMLElement>(
-        ".accordion__item-content"
+        '.accordion__item-content'
       );
 
       panel.style.height = `${pannelContent?.offsetHeight}px`;
 
-      setTimeout(() => (panel.style.height = "auto"), 250);
+      setTimeout(() => (panel.style.height = 'auto'), 250);
     }
   }
 
   closeItem(trigger: HTMLButtonElement) {
-    trigger.setAttribute("aria-expanded", "false");
+    trigger.setAttribute('aria-expanded', 'false');
 
     const panel = document.getElementById(
-      trigger.getAttribute("aria-controls") as string
+      trigger.getAttribute('aria-controls') as string
     );
 
     if (panel) {
       const pannelContent = panel.querySelector<HTMLElement>(
-        ".accordion__item-content"
+        '.accordion__item-content'
       );
       panel.style.height = `${pannelContent?.offsetHeight}px`;
-      setTimeout(() => (panel.style.height = "0px"));
+      setTimeout(() => (panel.style.height = '0px'));
     }
   }
 }
--- src/stories/modules/accordion.stories.ts
+++ src/stories/modules/accordion.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/html';
+import { v4 as uuidv4 } from 'uuid';
 import template from '../../html/modules/accordion.hbs';
 import { Link, ButtonProps } from '../components/button.stories';
 
@@ -7,6 +8,7 @@ export type AccordionProps = {
   title: string;
   description: string;
   items: {
+    id: string;
     title: string;
     heading: string;
     copy: string;
@@ -42,12 +44,14 @@ export const Default: Story = {
       'Praesent dui elit, porttitor sed vulputate id, mollis sed nibh. Morbi molestie scelerisque diam, at efficitur erat auctor vitae. Proin vehicula volutpat consequat. Donec sit amet magna sed urna egestas feugiat in et nisi. Suspendisse risus dolor, maximus sit amet pharetra sed, convallis sed orci. Nulla sit amet nisi cursus, blandit ipsum vel, feugiat nisl.',
     items: [
       {
+        id: uuidv4(),
         title: 'Accordion Tab Title',
         heading: 'Accordion Tab Heading',
         copy: 'Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.',
         cta: Link.args as ButtonProps
       },
       {
+        id: uuidv4(),
         title: 'Accordion Tab With Image',
         heading: 'Accordion Tab Heading',
         copy: 'Nullam quis risus eget urna mollis ornare vel eu leo. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.<br><img src="/images/image.jpeg" alt="Image Description" loading="lazy" />'
